e9a761f6c4132f43e9c502289d72fe5e8a20a6b1
Ilya Kucheryavyy <captain34690@gmail.com>
2022-07-11 19:49:53 +0300

    Renemed method

--- ProjectForFun/Components/DropDown.cs
+++ ProjectForFun/Components/DropDown.cs
@@ -9,7 +9,7 @@ namespace TestProjectEtsy.Components
             return Driver.FindElement(By.XPath($".//div[@class = '{dropdownIdentifier}']//option[contains(text(),'{optionName}')]"));
         }
 
-        public IWebElement GetOptionsDropdownByDropdownId(string dropdownId, string parameterName)
+        public IWebElement GetOptionsFromDropdownByDropdownId(string dropdownId, string parameterName)
         {
             return Driver.FindElement(By.XPath($".//div[@data-dropdown-id = '{dropdownId}']//li[contains(text(),'{parameterName}')]"));
         }
